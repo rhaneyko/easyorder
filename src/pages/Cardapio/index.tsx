d5ff5c0bac1435d399b3f261f0837ab2c36f6f4f
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-19 22:00:21 -0400

    Criação da pagina do Cardapio.

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Cardapio = () => {
+    return (
+        <Container>
+            Cardapio
+        </Container>
+    )
+};
+
+export default Cardapio;
