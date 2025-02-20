db10532e4f85c337b1e7d2f30b15904709a49f1a
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-19 22:00:56 -0400

    Criação da pagina de mesas.

---
+++ src/pages/Mesas/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Mesas = () => {
+    return (
+        <Container>
+            Mesas
+        </Container>
+    )
+};
+
+export default Mesas;
