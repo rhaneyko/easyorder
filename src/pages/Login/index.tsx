920a16f06af77d6cac2ccfe6bb22b14e52476577
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-19 22:00:45 -0400

    Criação da pagina de login.

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Login = () => {
+    return (
+        <Container>
+            <h1>Olá, (NOME DO USUARIO). Bem vindo ao sistema de pedidos. Vamos começar anotar seus pedidos?</h1>
+        </Container>
+    )
+};
+
+export default Login;
