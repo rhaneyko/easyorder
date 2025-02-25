413ffa1e085e01bed6ed44799bc1405c18e9361b
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-24 23:03:37 -0400

    Organização de código.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { IsMobile } from "../../hooks/use-mobile";
 
 import Header from "../Header";
 
 import { Container } from "./styles";
-import Sidebar from "../Sidebar";
 
 const Layout = () => {
     return (
         <Container>
-            {!IsMobile ? (
-                <Sidebar />
-            ) : (
-                <Header />
-            )}
+            <Header />
             <Outlet />
         </Container>
     )
