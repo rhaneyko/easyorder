c5ba241ad59b1bff3e9b6959b42e8738a33f2b20
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-25 22:30:54 -0400

    Criando os cards e adicionando o grafico.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,88 +1,31 @@
-import React from "react";
+import React from 'react';
+import { Container, ContainerHoje, PedidosHoje, TicketMedio, VendasHoje } from './styles';
+import BarChartComponent from "../../components/BarChart/BarChartComponent";
 
-import {
-    CardsContainer,
-    ContainerDashboard,
-    LeftCardsContainer,
-    MesaCard,
-    MesasContainer,
-    MesasContent,
-    PedidosContainer,
-    PedidosEmAndamentoList,
-    PedidoCard,
-    RightCardsContainer,
-    RightCardsContent,
-    VendasCard,
-    PedidosCard,
-    SummaryHeader
-} from "./styles";
 
 const Dashboard = () => {
-    const pedidosEmAndamento = [
-        { pedido: "01", mesa: "01", cliente: "Rhaneyko", garcom: "João Victor Bojikian" },
-        { pedido: "02", mesa: "02", cliente: "Rhaneyko", garcom: "João Victor Bojikian" },
-        { pedido: "36", mesa: "03", cliente: "Rhaneyko", garcom: "João Victor Bojikian" },
-        { pedido: "04", mesa: "04", cliente: "Rhaneyko", garcom: "João Victor Bojikian" },
-    ];
+  return (
+    <Container>
+        <h1>Dashboard</h1>
+        <ContainerHoje>
+            <VendasHoje>
+                <h2>Vendas Hoje</h2>
+                <p>R$ 1.000,00</p>
+            </VendasHoje>
+            <PedidosHoje>
+                <h2>Pedidos Hoje</h2>
+                <p>90</p>
+            </PedidosHoje>
+            <TicketMedio>
+                <h2>Ticket Médio</h2>
+                <p>R$ 100,00</p>
+            </TicketMedio>
+        </ContainerHoje>
+        <h2>Vendas da semana</h2>
+        <BarChartComponent />
+    </Container>
 
-    const mesas = [
-        { mesa: "01", pedido: "01", garcom: "João Victor Bojikian" },
-        { mesa: "02", pedido: "02", garcom: "João Victor Bojikian" },
-        { mesa: "03", pedido: "03", garcom: "João Victor Bojikian" },
-        { mesa: "04", pedido: "04", garcom: "João Victor Bojikian" },
-    ];
+  )
+}
 
-    return (
-        <ContainerDashboard>
-            <CardsContainer>
-                <LeftCardsContainer>
-                    <PedidosContainer>
-                        <h3>Pedidos em andamento</h3>
-                        <PedidosEmAndamentoList>
-                            {pedidosEmAndamento.map(({ pedido, mesa, cliente, garcom }) => (
-                                <PedidoCard key={pedido}>
-                                    <span><strong>Pedido:</strong> {pedido}</span>
-                                    <span><strong>Mesa:</strong> {mesa}</span>
-                                    <span><strong>Cliente:</strong> {cliente}</span>
-                                    <span><strong>Garçom:</strong> {garcom}</span>
-                                </PedidoCard>
-                            ))}
-                        </PedidosEmAndamentoList>
-                    </PedidosContainer>
-
-                    <MesasContainer>
-                        <h3>Mesas</h3>
-                        <MesasContent>
-                            {mesas.map(({ mesa, pedido, garcom }) => (
-                                <MesaCard key={mesa}>
-                                    <span><strong>Mesa:</strong> {mesa}</span>
-                                    <span><strong>Pedido:</strong> {pedido}</span>
-                                    <span><strong>Garçom:</strong> {garcom}</span>
-                                </MesaCard>
-                            ))}
-                        </MesasContent>
-                    </MesasContainer>
-                </LeftCardsContainer>
-
-                <RightCardsContainer>
-                    <SummaryHeader>
-                        <h3>Resumo do dia</h3>
-                    </SummaryHeader>
-                    <RightCardsContent>
-                        <VendasCard>
-                            <h4>Vendas do dia</h4>
-                            <span>Valor total: R$ 1.000,00</span>
-                            <span>Quantidade: 100</span>
-                        </VendasCard>
-                        <PedidosCard>
-                            <h4>Total de pedidos</h4>
-                            <span>Quantidade: 100</span>
-                        </PedidosCard>
-                    </RightCardsContent>
-                </RightCardsContainer>
-            </CardsContainer>
-        </ContainerDashboard>
-    )
-};
-
-export default Dashboard;
+export default Dashboard
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const ContainerDashboard = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -9,127 +9,68 @@ export const ContainerDashboard = styled.div`
   gap: 20px;
 `;
 
-export const CardsContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px;
-  padding: 32px;
-  background-color: red;
-
-
+export const ContainerHoje = styled.div`
+  display: flex;
+  justify-content: space-around;
+  gap: 50px;
 `;
 
-export const LeftCardsContainer = styled.div`
+export const VendasHoje = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  gap: 20px;
-`;
-
-export const RightCardsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: 10px;
-  gap: 20px;
-  
-
-`;
-
-export const RightCardsContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  height: 100%;
-  background-color: ${({ theme }) => theme.colors.cardBackground};
-  padding: 20px;
+  width: 250px;
+  height: 100px;
+  background-color: #D1D1D1;
   border-radius: 10px;
-`;
-
-export const MesasContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: 350px;
   padding: 10px;
-  background-color: ${({ theme }) => theme.colors.background};
+
+  span {
+    font-size: 1.7rem;
+    color: #fff;
+  }
+
+  p {
+    margin-top: -10px;
+    font-size: 2rem;
+  }
 `;
 
-export const MesasContent = styled.div`
-  display: grid;
-  width: 100%;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-  gap: 10px;
-`;
-
-export const MesaCard = styled.div`
+export const PedidosHoje = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 10px;
-  margin: 5px;
-  gap: 10px;
-  background-color: ${({ theme }) => theme.colors.cardBackground};
-  border-radius: 10px 0 10px 0;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-`;
-
-export const PedidosContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: 350px;
-  padding: 10px;
-  background-color: ${({ theme }) => theme.colors.background};
-`;
-
-export const PedidosEmAndamentoList = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-  width: 100%;
-  gap: 10px;
-`;
-
-export const PedidoCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 10px;
-  margin: 5px;
-  gap: 10px;
-  background-color: ${({ theme }) => theme.colors.cardBackground};
-  border-radius: 10px 0 10px 0;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-`;
-
-export const VendasCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  gap: 10px;
-  background-color: ${({ theme }) => theme.colors.primary};
-  color: white;
+  width: 250px;
+  height: 100px;
+  background-color: #D1D1D1;
   border-radius: 10px;
+  padding: 10px;
+
+  span {
+    font-size: 1.7rem;
+    color: #fff;
+  }
+
+  p {
+    margin-top: -10px;
+    font-size: 2rem;
+  }
 `;
 
-export const PedidosCard = styled.div`
+export const TicketMedio = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 20px;
-  gap: 10px;
-  background-color: ${({ theme }) => theme.colors.secondary};
-  color: white;
+  width: 250px;
+  height: 100px;
+  background-color: #D1D1D1;
   border-radius: 10px;
-`;
-
-export const SummaryHeader = styled.div`
-  display: flex;
-  justify-content: center;
   padding: 10px;
-  background-color: ${({ theme }) => theme.colors.background};
-  border-radius: 10px;
-  margin-bottom: 20px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+
+  span {
+    font-size: 1.7rem;
+    color: #fff;
+  }
+
+  p {
+    margin-top: -10px;
+    font-size: 2rem;
+  }
 `;
