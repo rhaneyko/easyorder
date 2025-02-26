16e7ab2f06ee13e965d464f548f1f6656e55a24f
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-25 22:31:24 -0400

    Criação dos cards das mesas.

--- src/pages/Mesas/index.tsx
+++ src/pages/Mesas/index.tsx
@@ -1,11 +1,94 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+    Container,
+    MesaCardLivre,
+    MesaCardOcupada,
+    MesasContainer
+} from "./styles";
+
+const mesas = [
+    {
+        id: 1,
+        status: "Ocupada",
+        tempo: "41 min"
+    },
+    {
+        id: 2,
+        status: "Livre"
+    },
+    {
+        id: 3,
+        status: "Livre",
+        tempo: "15 min"
+    },
+    {
+        id: 4,
+        status: "Ocupada"
+    },
+    {
+        id: 5,
+        status: "Ocupada",
+        tempo: "30 min"
+    },
+    {
+        id: 6,
+        status: "Livre"
+    },
+    {
+        id: 7,
+        status: "Livre",
+        tempo: "10 min"
+    },
+    {
+        id: 8,
+        status: "Livre"
+    },
+    {
+        id: 9,
+        status: "Ocupada",
+        tempo: "20 min"
+    },
+    {
+        id: 10,
+        status: "Livre"
+    },
+    {
+        id: 11,
+        status: "Ocupada",
+        tempo: "25 min"
+    },
+    {
+        id: 12,
+        status: "Ocupada"
+    },
+]
 
 const Mesas = () => {
     return (
         <Container>
-            Mesas
+            <h1>Mesas</h1>
+            <MesasContainer>
+                {mesas.map((mesa) => {
+                    if (mesa.status === "Ocupada") {
+                        return (
+                            <MesaCardOcupada key={mesa.id}>
+                                <span>Mesa {mesa.id}</span>
+                                <span>{mesa.status}</span>
+                                <span>{mesa.tempo}</span>
+                            </MesaCardOcupada>
+                        )
+                    } else {
+                        return (
+                            <MesaCardLivre key={mesa.id}>
+                                <span>Mesa {mesa.id}</span>
+                                <span>{mesa.status}</span>
+                            </MesaCardLivre>
+                        )
+                    }
+                }
+                )}
+            </MesasContainer>
         </Container>
     )
 };
--- src/pages/Mesas/styles.ts
+++ src/pages/Mesas/styles.ts
@@ -1,3 +1,50 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    padding: 20px;
+    margin-top: 50px;
+`;
+
+export const MesasContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 20px;
+`;
+
+export const MesaCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    height: 150px;
+    border-radius: 10px;
+    background-color:rgb(223, 106, 28);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    transition: 0.3s;
+
+    &:hover {
+        transform: scale(1.05);
+    }
+
+    span {
+        font-size: 1rem;
+        font-weight: 700;
+        margin-bottom: 10px;
+    }
+`;
+
+export const MesaCardOcupada = styled(MesaCard)`
+    background-color: #ff0000;	    
+`;
+
+export const MesaCardLivre = styled(MesaCard)`
+    background-color: #00ff00;          
+`;
+
