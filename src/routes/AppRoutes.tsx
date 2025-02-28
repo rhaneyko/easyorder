c40ffebb11cc6c29e3c1b0a5ab18413a58205328
rhaneyko <rhaneykohonorio@gmail.com>
2025-02-27 21:18:44 -0400

    Adicionando as telas de login e registro as rotas.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,21 +2,28 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "../components/Layout";
+import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
 import Dashboard from "../pages/Dashboard";
 import Pedidos from "../pages/Pedidos";
 import Mesas from "../pages/Mesas";
 import Cardapio from "../pages/Cardapio";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRoutes: React.FC = () => {
     return (
         <Router>
             <Routes>
+                {/* <Route path="/" element={<ProtectedRoute />}> */}
                 <Route path="/" element={<Layout />}>
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/pedidos" element={<Pedidos />} />
                     <Route path="/mesas" element={<Mesas />} />
                     <Route path="/cardapio" element={<Cardapio />} />
                 </Route>
+                {/* </Route> */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     )
