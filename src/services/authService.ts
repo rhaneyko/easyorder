7516c00e55e7918e55973c4055314d2f27352ebb
rhaneyko <rhaneykohonorio@gmail.com>
2025-03-02 22:42:59 -0400

    Alterando o username para email.

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,10 +1,10 @@
+// src/services/authService.ts
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { auth, db } from "../api/firebaseConfig";
 
 interface UserData {
   nome: string;
-  username: string;
   email: string;
   senha: string;
   tipoUsuario: string;
@@ -12,7 +12,7 @@ interface UserData {
 
 // Registrar usuário e salvar no Firestore
 export const registerUserWithRole = async (userData: UserData) => {
-  const { nome, username, email, senha, tipoUsuario } = userData;
+  const { nome, email, senha, tipoUsuario } = userData;
 
   // Criar usuário no Firebase Authentication
   const userCredential = await createUserWithEmailAndPassword(auth, email, senha);
@@ -21,9 +21,8 @@ export const registerUserWithRole = async (userData: UserData) => {
   // Criar documento no Firestore
   await setDoc(doc(db, "users", user.uid), {
     nome,
-    username,
+    email,
     userType: tipoUsuario,
-    email, // Certifique-se de que o email está sendo salvo
     createdAt: new Date(),
   });
 };
