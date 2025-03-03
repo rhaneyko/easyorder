feaeb709a09cabc2dbc0b0603ec696f248ebb0ee
rhaneyko <rhaneykohonorio@gmail.com>
2025-03-02 22:41:52 -0400

    Adicionando uma proteção de rotas, apenas usuarios logados conseguem acessar as demais telas.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -14,14 +14,14 @@ const AppRoutes: React.FC = () => {
     return (
         <Router>
             <Routes>
-                {/* <Route path="/" element={<ProtectedRoute />}> */}
+                <Route path="/" element={<ProtectedRoute />}>
                 <Route path="/" element={<Layout />}>
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/pedidos" element={<Pedidos />} />
                     <Route path="/mesas" element={<Mesas />} />
                     <Route path="/cardapio" element={<Cardapio />} />
                 </Route>
-                {/* </Route> */}
+                </Route>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
             </Routes>
